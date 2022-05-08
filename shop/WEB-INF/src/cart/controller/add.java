570428e89e185dc1b74ddd6b44dcb5c66df527cf
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-05-08 20:23:07 +0900

    cart画面の追加リンクのモック画面遷移

---
+++ shop/WEB-INF/src/cart/controller/add.java
@@ -0,0 +1,25 @@
+package cart.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/cart/add")
+public class add extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException {
+    response.setContentType("text/html; charset=UTF-8");
+    request.setCharacterEncoding("UTF-8");
+
+    String id = request.getParameter("id");
+
+    System.out.println("商品ID: " + id);
+
+    response.sendRedirect("index");
+  }
+}

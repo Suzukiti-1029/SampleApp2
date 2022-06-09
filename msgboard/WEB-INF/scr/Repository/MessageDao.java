8ecd3ac266831bfc12dd9b7026123ea319da29cd
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-06-10 07:35:28 +0900

    入力されたデータをDBに追加

--- msgboard/WEB-INF/scr/Repository/MessageDao.java
+++ msgboard/WEB-INF/scr/Repository/MessageDao.java
@@ -6,9 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import Entity.Message;
 import jakarta.servlet.ServletException;
@@ -17,9 +15,6 @@ public class MessageDao {
   // JSP用
   public MessageDao() {
   }
-
-  // 仮DB
-  private static List<Message> messages_old = new ArrayList<Message>();
 
   public List<Message> select() throws ServletException {
     Connection con = null;
@@ -31,7 +26,7 @@ public class MessageDao {
       Class.forName("com.mysql.jdbc.Driver");
       // データベースへ接続
       con = DriverManager.getConnection(
-        "jdbc:mysql://msgboard-mysql-docker:3306/messageapp?useUnicode=true&characterEncoding=utf-8&characterSetResults=utf-8",
+        "jdbc:mysql://msgboard-mysql-docker:3306/messageapp",
         "root",
         "256133"
       );
@@ -60,53 +55,33 @@ public class MessageDao {
     return messages;
   }
 
-  public void save(Message message) {
-    messages_old.add(message);
-  }
-
-
-
-
-
-
-
-
-
-
-  public Map<String, Object> connectDB() throws ServletException {
+  public void save(Message message) throws ServletException {
     Connection con = null;
     PreparedStatement pstmt = null;
-    ResultSet rs = null;
-    Map<String, Object> data = new HashMap<String, Object>();
-
+    
     try {
       Class.forName("com.mysql.jdbc.Driver");
       // データベースへ接続
       con = DriverManager.getConnection(
-        "jdbc:mysql://msgboard-mysql-docker:3306/messageapp?useUnicode=true&characterEncoding=utf8",
-        "root",
-        "256133"
-      );
+          "jdbc:mysql://msgboard-mysql-docker:3306/messageapp",
+          "root",
+          "256133");
 
       // SQLの実行
-      pstmt = con.prepareStatement("select id,name from messages");
-      rs = pstmt.executeQuery();
-
-      // Viewへ引き渡す値を設定
-      while (rs.next()) {
-        data.put("id", rs.getInt("id"));
-        data.put("name", rs.getString("name"));
-      }
+      String sql = "insert into messages(title, contents) values (?, ?)";
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, message.getTitle());
+      pstmt.setString(2, message.getContents());
+      pstmt.executeUpdate();
+      pstmt.clearParameters();
     } catch (Exception e) {
       throw new ServletException(e);
     } finally {
       try {
-        rs.close();
         pstmt.close();
         con.close();
       } catch (Exception e) {
       }
     }
-    return data;
   }
 }

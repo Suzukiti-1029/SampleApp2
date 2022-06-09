38cdb17a54e3723c70e03d488e707fad2ebc052c
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-06-10 08:29:14 +0900

    文字コード指定をフィルタ化

--- msgboard/WEB-INF/scr/Controller/indexController.java
+++ msgboard/WEB-INF/scr/Controller/indexController.java
@@ -14,9 +14,6 @@ public class indexController extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
   throws IOException, ServletException {
-    response.setContentType("text/html; charset=UTF-8");
-    request.setCharacterEncoding("UTF-8");
-
     messageService service = new messageService();
     service.getMessages(request);
 
---
+++ msgboard/WEB-INF/scr/Filter/commonFilter.java
@@ -0,0 +1,26 @@
+package Filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/*")
+public class commonFilter implements Filter {
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+  throws IOException, ServletException {
+    HttpServletRequest hRequest = (HttpServletRequest)request;
+    HttpServletResponse hResponse = (HttpServletResponse)response;
+
+    hRequest.setCharacterEncoding("UTF-8");
+    hResponse.setCharacterEncoding("UTF-8");
+
+    chain.doFilter(hRequest, hResponse);
+  }
+}
